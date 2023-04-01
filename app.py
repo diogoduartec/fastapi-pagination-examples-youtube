0b64601e402e9eff86e9840795b338aec554bd68
Diogo Duarte <duartediogo98@gmail.com>
2023-03-31 21:34:39 -0400

    add static pagination

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends
+from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage
 from db import SessionLocal, ProductModel
 from schemas import ProductOutput
 
@@ -14,3 +15,16 @@ def get_products():
     ]
 
     return fake_products
+
+
+@app.get('/products/paginate/default', response_model=Page[ProductOutput])
+@app.get('/products/paginate/limit-offset', response_model=LimitOffsetPage[ProductOutput])
+def get_products():
+    fake_products = [
+        ProductOutput(name=f'Flamengo {n}', price=n * .1, id=n)
+        for n in range(100)
+    ]
+
+    return paginate(fake_products)
+
+add_pagination(app)
